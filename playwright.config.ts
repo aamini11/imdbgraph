d9862b4bfbfccab5180132a8ca8e891cc976837e
Sahand Amini <sahand775@gmail.com>
2025-07-07 02:34:40 -0400

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,6 @@ export default defineConfig({
       NODE_ENV: "test",
       DATABASE_URL: process.env.DATABASE_URL ?? "",
     },
-    stdout: "pipe",
   },
 
   /* Configure projects for major browsers */
